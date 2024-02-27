b909912cf3719687912e0638c051e89cad419506
Mahib09 <Mahib.Ranamagar@triosstudent.com>
2024-02-27 14:59:26 -0500

    about section layouting done

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
-//scroll navbar
-myID = document.getElementById("hidnav");
 
-var myScrollFunc = function() {
-  var y = window.scrollY;
-  if (y >= 200) {
-    myID.className = "box show"
-  } else {
-    myID.className = "box hide"
-  }
-};
-window.addEventListener("scroll", myScrollFunc);
 
